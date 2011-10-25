80a2f5cf2d180fe29e2f33355d305e1df203db05
Dave Gamache <dhgamache@gmail.com>
2011-10-24 21:44:37 -0700

    redesign starting to take shape a bit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*
+* Skeleton V1.1
+* Copyright 2011, Dave Gamache
+* www.getskeleton.com
+* Free to use under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+* 8/17/2011
+*/
+
+$(document).ready(function() {
+
+  $('body').addClass('rendered')
+
+
+});
