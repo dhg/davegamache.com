d90e59e480e5d904377dc1655d0f0b320fb98c11
Dave Gamache <dhgamache@twitter.com>
2012-06-24 10:43:38 -0700

    changing this repo to only be the theme for davegamache.com

---
+++ footer.php
@@ -0,0 +1,39 @@
+   <footer class="footer">
+      <div class="back-to-top">
+        <a href="">Back to top</a>
+      </div>
+      <div class="footer-nav">
+        <?php echo previous_post_link('%link', 'Previous'); ?>
+        <?php if(get_adjacent_post(false, '', true) && get_adjacent_post(false, '', false)) {
+          echo " / ";
+        } ?>
+        <?php echo next_post_link('%link', 'Next'); ?>
+        &nbsp; <!-- here in case there is nothing here to still occupy space -->
+      </div>
+      <aside class="offsite-links">
+        <a class="twitter-link" href="http://www.twitter.com/dhg">Twitter</a> /
+        <a class="dribbble-link" href="http://dribbble.com/dhg">Dribbble</a> /
+        <a class="rss-link" href="<?php bloginfo('rss2_url'); ?>">RSS</a>
+      </aside>
+    </footer>
+
+  </div> <!-- / .wrapper -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20752697-1']);
+  _gaq.push(['_setDomainName', '.davegamache.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
-?>
+<?php get_header(); ?>
+<?php get_template_part( 'loop', 'index' ); ?>
+<?php get_footer(); ?>
